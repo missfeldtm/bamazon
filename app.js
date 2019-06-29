3752a8491fb30cde3cff66d5ad174321ff471738
Mckenna <missfeldtm@gmail.com>
2019-06-28 21:09:27 -0500

    second part done

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ var mysql = require("mysql");
 
 var keys = require("./keys.js");
 var mySQLPassword = keys.mySQLkey;
-
-
-
-
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -24,12 +20,68 @@ var connection = mysql.createConnection({
     password: mySQLPassword,
     database: "bamazon_db"
 });
-
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    displayInventory()
 });
+
+function validateInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a whole non-zero number.';
+    }
+}
+
+
+
+/**** Display User Options ****/
+
+inquirer.prompt([{
+        type: 'input',
+        name: 'item_id',
+        message: 'Please enter the Item ID which you would like to purchase.',
+        validate: validateInput,
+        filter: Number
+    },
+    {
+        type: 'input',
+        name: 'quantity',
+        message: 'How many do you need?',
+        validate: validateInput,
+        filter: Number
+    }
+]).then(function (input) {
+    console.log('Customer has selected: \n    item_id = ' + input.item_id + '\n    quantity = ' + input.quantity);
+
+    var item = input.item_id;
+    var quantity = input.quantity;
+
+    var queryStr = 'SELECT * FROM products WHERE ?';
+
+    connection.query(queryStr, {item_id: item}, function(err, data) {
+        if (err) throw err;
+
+        // If the user has selected an invalid item ID, data attay will be empty
+        // console.log('data = ' + JSON.stringify(data));
+
+        if (data.length === 0) {
+            console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+            displayInventory();
+
+        }
+
+        /*** */
+    })
+    
+
+
+    /**** */
+});
+
 
 function displayInventory() {
 
@@ -64,3 +116,16 @@ function displayInventory() {
 
     })
 }
+
+/*
+function runBamazon() {
+
+
+	// Display the available inventory
+	displayInventory();
+}
+
+
+runBamazon();
+
+*/
